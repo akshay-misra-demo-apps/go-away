b3bdad74e47cabd8bbd85f0358e219a6dbfc0e3b
akshay-misra-demo-apps <akshaymishra2492@gmail.com>
2023-09-15 10:09:50 +0530

    first commit

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math/big"
+	"reflect"
 )
 
 func init() {
@@ -14,4 +16,20 @@ func init() {
 
 func main() {
 	fmt.Println("Hello go")
+
+	y := 10
+
+	z := 10.0
+
+	fmt.Println("Y", reflect.TypeOf(y))
+	fmt.Println("Z", reflect.TypeOf(z))
+
+	// Create a new big integer
+	bigInt := new(big.Int)
+
+	// Set a value
+	bigInt.SetString("5234567890123456789012371678901234567890", 10)
+
+	// Perform arithmetic operations and other operations with bigInt
+	fmt.Println(bigInt)
 }
